7c38fe6fc091855cb13172c5539845149ff48320
Kelly Tran <kellytran03@berkeley.edu>
2025-01-31 19:44:02 -0800

    Updates

---
+++ precache-manifest.cd9e64b7c667ae288329d19deef58b38.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76ff61c16cc7eabcd4e7e821580c0b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dc747fbd92ff70daf7a",
+    "url": "/static/css/main.e5ca16d2.chunk.css"
+  },
+  {
+    "revision": "8befb1b5cc55ea8a3cad",
+    "url": "/static/js/2.f0a20458.chunk.js"
+  },
+  {
+    "revision": "4dd206dec5ff825e678a",
+    "url": "/static/js/3.2ebb6cf0.chunk.js"
+  },
+  {
+    "revision": "3dc747fbd92ff70daf7a",
+    "url": "/static/js/main.b0017749.chunk.js"
+  },
+  {
+    "revision": "7540571c425f2fb37175",
+    "url": "/static/js/runtime~main.66990ebe.js"
+  },
+  {
+    "revision": "d18ecf6a00e19df06b8b42f22d989d4c",
+    "url": "/static/media/annie.3dce1c59.jpg"
+  },
+  {
+    "revision": "152b2f6a16c8171a2e350e229add0071",
+    "url": "/static/media/braindump.8dbf0a02.png"
+  },
+  {
+    "revision": "4c501126a7a6ced4624549f0ba889ee6",
+    "url": "/static/media/cabrillo.4fb8a049.jpeg"
+  },
+  {
+    "revision": "3bef6c7c5aef766fc7654af1d7286c48",
+    "url": "/static/media/cal-day.bdfacd86.jpg"
+  },
+  {
+    "revision": "fa2ea6a2c1f351fdc675f59e7dd9e675",
+    "url": "/static/media/cd.fa2ea6a2.svg"
+  },
+  {
+    "revision": "a642bcba9f037ac2e89dacbae6ef650a",
+    "url": "/static/media/chat.379056cc.png"
+  },
+  {
+    "revision": "bf5aa4b5275f744826b7f7b646de752a",
+    "url": "/static/media/coffee-wifi.bf5aa4b5.svg"
+  },
+  {
+    "revision": "cef326251d6bef59ef6e8c68c50879f1",
+    "url": "/static/media/concert.ba0055cf.jpg"
+  },
+  {
+    "revision": "959ca95e8a457897bdd6e7e63319f5f2",
+    "url": "/static/media/diamond1.959ca95e.webp"
+  },
+  {
+    "revision": "d59e4f3dac33d16f46c0b4ebf1d8ae85",
+    "url": "/static/media/glade.af754e85.jpg"
+  },
+  {
+    "revision": "794ac543440c459685d9ed5046202be0",
+    "url": "/static/media/jenny.f2c2add1.jpg"
+  },
+  {
+    "revision": "7c4c74a5081d408016c8307f59aecf76",
+    "url": "/static/media/kelly-headshot.a276b334.jpg"
+  },
+  {
+    "revision": "f1a3fbfab70aa24befbffc2932a568d1",
+    "url": "/static/media/matcha.b20302d0.jpg"
+  },
+  {
+    "revision": "1ed3ea4f52b65be43e52809202c682fa",
+    "url": "/static/media/pwc.50506881.jpeg"
+  },
+  {
+    "revision": "60d70f943a69b902a21ca98ec0c3a7b3",
+    "url": "/static/media/record_player.60d70f94.svg"
+  },
+  {
+    "revision": "4356a67a077755694f243f1b2e1d081e",
+    "url": "/static/media/saas.e8f102c5.jpg"
+  },
+  {
+    "revision": "491f4fd0518b5d7897c2b16d5aa887ff",
+    "url": "/static/media/silent_drives.b8e3da33.png"
+  },
+  {
+    "revision": "a08256f38c9a9023f6bbaa4549f2e9c9",
+    "url": "/static/media/sophme.08a9792f.jpg"
+  },
+  {
+    "revision": "ff24368016f1382b5907a172cb79f45a",
+    "url": "/static/media/teaparty.06ce556f.jpg"
+  },
+  {
+    "revision": "91e99d29fcb20aede032c9978bc67422",
+    "url": "/static/media/um.7e1375cf.jpeg"
+  },
+  {
+    "revision": "ff0922b82c6e554ef2b969f24c5be71c",
+    "url": "/static/media/wdd.15d87fbe.png"
+  }
+]);
